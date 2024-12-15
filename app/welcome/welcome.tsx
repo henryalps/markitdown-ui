6f358bdfa4d5e9f621ff1941fed4c68ddfdafeb2
Bruno Paulino <contact@bpaulino.com>
2024-12-14 22:07:45 -0300

    PoC React Router

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,9 @@
+import { Button } from "nes-ui-react";
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <Button color='warning' borderInverted>Click</Button>
+    </main>
+  );
+}
